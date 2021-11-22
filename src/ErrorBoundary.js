4ccd504a536c80065622b58987c42b16ba24798f
unknown <danielsiwik1@gmail.com>
2021-11-22 21:24:06 +0100

    first commit

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Zaktualizuj stan, aby następny render pokazał zastępcze UI.
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Możesz wyrenderować dowolny interfejs zastępczy.
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
